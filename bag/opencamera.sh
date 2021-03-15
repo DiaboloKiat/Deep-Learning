34abd9ce0a9dc1bd4ea54d011cb0c058cfb4dbb5
DiaboloKiat <DiaboloKiat@gmail.com>
2021-03-15 10:40:43 +0800

    realsense-ros

--- bag/opencamera.sh
+++ bag/opencamera.sh
@@ -1,3 +1,4 @@
 cd ~/Deep-Learning
 source environment.sh
+cd ~/Deep-Learning/bag
 roslaunch realsense2_camera rs_camera.launch
---
+++ catkin_make.sh
@@ -0,0 +1,4 @@
+cd ~/Deep-Learning/catkin_ws
+catkin_make
+source $HOME/Deep-Learning/catkin_ws/devel/setup.bash
+cd ~/Deep-Learning
